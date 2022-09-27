9790ed6ea25f355ac1a277d3eafe59a9b5219ac9
GarlicBuns19 <planterx26@gmail.com>
2022-09-27 16:29:23 +0200

    Line through

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,23 +1,56 @@
 <template>
   <section class="todoMain pa-6">
-    <v-list-item>
-      <template v-slot:default="{ active }">
-        <v-list-item-action>
-          <v-checkbox :input-value="active"></v-checkbox>
-        </v-list-item-action>
+    <div v-for="task in tasks" :key="task.id">
+      <v-list class="pt-0" flat>
+        <v-list-item 
+        @click="todoDone(task.id)" 
+        :class="{ 'blue lighten-5' : task.done}">
+          <template v-slot:default>
+            <v-list-item-action>
+              <v-checkbox :input-value="task.done" color:primary></v-checkbox>
+            </v-list-item-action>
 
-        <v-list-item-content>
-          <v-list-item-title>Notifications</v-list-item-title>
-          <v-list-item-subtitle
-            >Notify me about updates to apps or games that I
-            downloaded</v-list-item-subtitle
-          >
-        </v-list-item-content>
-      </template>
-    </v-list-item>
+            <v-list-item-content>
+              <v-list-item-title
+              :class="{'text-decoration-line-through' : task.done}"
+              >{{ task.title }}</v-list-item-title>
+            </v-list-item-content>
+          </template>
+        </v-list-item>
+      </v-list>
+      <v-divider></v-divider>
+    </div>
   </section>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          title: "Eat Food",
+          done: false,
+        },
+        {
+          id: 2,
+          title: "Milk",
+          done: false,
+        },
+        {
+          id: 3,
+          title: "Car",
+          done: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    todoDone(id) {
+      let task = this.tasks.filter((t) => t.id === id)[0];
+      task.done = !task.done;
+    },
+  },
+};
 </script>
