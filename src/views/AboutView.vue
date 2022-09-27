6b2755ba087f8f85e771f30ccb87f4319a959f6f
GarlicBuns19 <planterx26@gmail.com>
2022-09-27 13:58:16 +0200

    Added links

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About vuetify todo</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis explicabo quibusdam doloribus rerum neque odio repellat corporis libero itaque odit maiores quo quae, commodi error tempora. Quae numquam libero iusto?</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis explicabo quibusdam doloribus rerum neque odio repellat corporis libero itaque odit maiores quo quae, commodi error tempora. Quae numquam libero iusto?</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis explicabo quibusdam doloribus rerum neque odio repellat corporis libero itaque odit maiores quo quae, commodi error tempora. Quae numquam libero iusto?</p>
   </div>
 </template>
