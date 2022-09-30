5c86cabfd8b54af9fdc7ebd8e9b16965db687760
GarlicBuns19 <planterx26@gmail.com>
2022-09-30 12:20:01 +0200

    Null tasks

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -11,34 +11,37 @@
       @click:append="addTask()"
       @keyup.enter="addTask()"
     ></v-text-field>
-    <div v-for="task in tasks" :key="task.id">
-      <v-list class="pt-0" flat>
-        <v-list-item :class="{ 'blue lighten-5': task.done }">
-          <template v-slot:default>
-            <v-list-item-action>
-              <v-checkbox
-                :input-value="task.done"
-                @click="todoDone(task.id)"
-                color:primary
-              ></v-checkbox>
-            </v-list-item-action>
+    <div v-if="tasks">
+      <div v-for="task in tasks" :key="task.id">
+        <v-list class="pt-0" flat>
+          <v-list-item :class="{ 'blue lighten-5': task.done }">
+            <template v-slot:default>
+              <v-list-item-action>
+                <v-checkbox
+                  :input-value="task.done"
+                  @click="todoDone(task.id)"
+                  color:primary
+                ></v-checkbox>
+              </v-list-item-action>
 
-            <v-list-item-content>
-              <v-list-item-title
-                :class="{ 'text-decoration-line-through': task.done }"
-                >{{ task.title }}</v-list-item-title
-              >
-            </v-list-item-content>
-            <v-list-item-action>
-              <v-btn @click="deleteTodo(task.id)" icon>
-                <v-icon color="primary lighten-1">mdi-delete</v-icon>
-              </v-btn>
-            </v-list-item-action>
-          </template>
-        </v-list-item>
-      </v-list>
-      <v-divider></v-divider>
+              <v-list-item-content>
+                <v-list-item-title
+                  :class="{ 'text-decoration-line-through': task.done }"
+                  >{{ task.title }}</v-list-item-title
+                >
+              </v-list-item-content>
+              <v-list-item-action>
+                <v-btn @click="deleteTodo(task.id)" icon>
+                  <v-icon color="primary lighten-1">mdi-delete</v-icon>
+                </v-btn>
+              </v-list-item-action>
+            </template>
+          </v-list-item>
+        </v-list>
+        <v-divider></v-divider>
+      </div>
     </div>
+    <div v-else>Nothing tfgvbhjnmk</div>
   </section>
 </template>
 
@@ -78,7 +81,7 @@ export default {
         title: `${newTask}`,
         done: false,
       });
-      this.newTask = ''
+      this.newTask = "";
     },
     deleteTodo(id) {
       console.log(id);
