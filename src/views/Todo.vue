25c4fd4f2ca7198a2f70358db5bacc5a09fbbabe
GarlicBuns19 <planterx26@gmail.com>
2022-10-01 12:01:18 +0100

    Edit task

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -8,7 +8,7 @@
       append-icon="mdi-message-plus-outline"
       hide-details
       clearable
-      @click:append="addTask(),taskAdded()"
+      @click:append="addTask(), taskAdded()"
       @keyup.enter="addTask()"
     ></v-text-field>
     <div v-if="tasks">
@@ -31,7 +31,49 @@
                 >
               </v-list-item-content>
               <v-list-item-action>
-                <v-btn @click="deleteTodo(task.id)" icon>
+                <v-dialog v-model="dialog" persistent max-width="600px">
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn
+                      icon
+                      v-bind="attrs"
+                      v-on="on"
+                    >
+                      <v-icon color="primary lighten-1"
+                        >mdi-circle-edit-outline</v-icon
+                      >
+                    </v-btn>
+                  </template>
+                  <v-card>
+                    <v-card-text>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12" sm="6" md="4">
+                            <v-text-field
+                              label="Edit this task to"
+                              v-model="task.title"
+                            ></v-text-field>
+                          </v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card-text>
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn color="blue darken-1" text @click="dialog = false">
+                        Close
+                      </v-btn>
+                      <v-btn
+                        color="blue darken-1"
+                        @click="editTodo(task.id)"
+                        text
+                      >
+                        Edit
+                      </v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+              </v-list-item-action>
+              <v-list-item-action>
+                <v-btn @click="deleteTodo(task.id), taskDel()" icon>
                   <v-icon color="primary lighten-1">mdi-delete</v-icon>
                 </v-btn>
               </v-list-item-action>
@@ -42,7 +84,10 @@
       </div>
     </div>
     <center>
-      <h1 data-aos="fade-up" id="screenBottom">Task added</h1>
+      <h1 data-aos="fade-up" id="tAdd">Task added</h1>
+    </center>
+    <center>
+      <h1 data-aos="fade-up" id="tDel">Task Deleted</h1>
     </center>
     <div v-if="tasks == ''" class="text-center">
       <div class="mt-16">
@@ -57,6 +102,7 @@
 export default {
   data() {
     return {
+      dialog : false ,
       newTask: "",
       tasks: [
         {
@@ -88,24 +134,43 @@ export default {
       console.log(id);
       this.tasks = this.tasks.filter((t) => t.id !== id);
     },
+    editTodo(id) {
+      console.log("This task has just been edited");
+      this.dialog = false;
+    },
     taskAdded() {
-      var x = document.getElementById("screenBottom");
-      if (x.style.display = "none") {
+      var x = document.getElementById("tAdd");
+      if ((x.style.display = "none")) {
         x.style.display = "block";
       }
-      if(x.style.display = "block"){
-        setTimeout(function gone(){
-            x.style.display = "none";
-        },1000)
-      } 
+      if ((x.style.display = "block")) {
+        setTimeout(function gone() {
+          x.style.display = "none";
+        }, 1000);
+      }
+    },
+    taskDel() {
+      var x = document.getElementById("tDel");
+      if ((x.style.display = "none")) {
+        x.style.display = "block";
+      }
+      if ((x.style.display = "block")) {
+        setTimeout(function gone() {
+          x.style.display = "none";
+        }, 1000);
+      }
     },
   },
 };
 </script>
+
 <style scoped>
-  #screenBottom{
-    display: none;
-  }
+#tAdd {
+  display: none;
+}
+#tDel {
+  display: none;
+}
 </style>
 
 
