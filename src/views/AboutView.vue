70e9d063ec0a0222c533ce29f9b605c2eb423ffd
GarlicBuns19 <planterx26@gmail.com>
2022-10-07 09:28:47 +0200

    Added color to about section

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="about pa-6">
-    <h1>About vuetify todo</h1>
-    <p>I created this todo app to practice vuetify</p>
+  <div class="about pa-6 text-center">
+    <h1 class="text-h3 pb-5">About vuetify todo</h1>
+    <p class="black--text">I created this todo app to practice vuetify</p>
+    <p class="black--text">I am currently up skilling this skill</p>
   </div>
 </template>
